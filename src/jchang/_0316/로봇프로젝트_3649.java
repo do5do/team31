b9fc03dde8c796d6805352e45bfa4d3fadef7517
nadoran78 <nadoran78@gmail.com>
2024-03-19 00:10:05 +0900

    0319

---
+++ src/jchang/_0316/로봇프로젝트_3649.java
@@ -0,0 +1,50 @@
+package jchang._0316;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 로봇프로젝트_3649 {
+
+  static int hole;
+  static int n;
+  static int[] lego;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String line = br.readLine();
+    while (line != null) {
+      hole = Integer.parseInt(line) * 10_000_000;
+      n = Integer.parseInt(br.readLine());
+      lego = new int[n];
+      for (int i = 0; i < n; i++) {
+        lego[i] = Integer.parseInt(br.readLine());
+      }
+
+      Arrays.sort(lego);
+
+      int left = 0;
+      int right = n - 1;
+      boolean flag = false;
+      while (left < right) {
+        if (lego[left] + lego[right] == hole) {
+          System.out.printf("yes %d %d%n", lego[left], lego[right]);
+          flag = true;
+          break;
+        } else if (lego[left] + lego[right] < hole) {
+          left++;
+        } else if (lego[left] + lego[right] > hole) {
+          right--;
+        }
+      }
+
+      if (!flag) {
+        System.out.println("danger");
+      }
+
+      line = br.readLine();
+    }
+
+  }
+}
---
+++ src/jchang/_0316/신입사원_1946.java
@@ -0,0 +1,67 @@
+package jchang._0316;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 신입사원_1946 {
+
+  static int T;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    T = Integer.parseInt(br.readLine());
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < T; i++) {
+
+      int apply = Integer.parseInt(br.readLine());
+      Node[] nodes = new Node[apply];
+      for (int k = 0; k < apply; k++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int document = Integer.parseInt(st.nextToken());
+        int interview = Integer.parseInt(st.nextToken());
+        nodes[k] = new Node(document, interview);
+      }
+
+      Arrays.sort(nodes);
+
+      int ans = 1;
+      Node min = nodes[0];
+      for (int j = 1; j < nodes.length; j++) {
+        Node cur = nodes[j];
+        if (cur.interview < min.interview) {
+          ans++;
+          min = cur;
+        }
+      }
+
+      sb.append(ans).append("\n");
+    }
+
+    System.out.println(sb);
+
+  }
+
+
+
+  static class Node implements Comparable<Node>{
+    int document;
+    int interview;
+    public Node(int document, int interview) {
+      this.document = document;
+      this.interview = interview;
+    }
+
+
+
+    @Override
+    public int compareTo(Node o) {
+      return Integer.compare(document, o.document);
+    }
+  }
+
+}

c59894288f3078d5f6f2293eface1192e49d5208
nadoran78 <nadoran78@gmail.com>
2024-03-28 18:30:26 +0900

    0328

---
+++ src/jchang/_0328/카드2_2164.java
@@ -0,0 +1,28 @@
+package jchang._0328;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class 카드2_2164 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    Deque<Integer> deque = new LinkedList<>();
+
+    for (int i = 1; i <= N ; i++) {
+      deque.add(i);
+    }
+
+    while (deque.size() > 1) {
+      deque.pollFirst();
+      deque.addLast(deque.pollFirst());
+    }
+
+    System.out.println(deque.peek());
+
+  }
+}

bcfb7ce6d72ea6b3e996514e6bc22c9cf76d8753
nadoran78 <nadoran78@gmail.com>
2024-04-02 23:04:31 +0900

    0402

---
+++ src/jchang/_0402/KCPC_3758.java
@@ -0,0 +1,77 @@
+package jchang._0402;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class KCPC_3758 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < T; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken()); // 팀의 개수
+      int k = Integer.parseInt(st.nextToken()); // 문제 개수
+      int t = Integer.parseInt(st.nextToken()); // 팀 ID
+      int m = Integer.parseInt(st.nextToken()); // 로그 엔트리 개수
+
+      Map<Integer, Node> map = new HashMap<>();
+      for (int j = 0; j < m; j++) {
+        st = new StringTokenizer(br.readLine());
+        int id = Integer.parseInt(st.nextToken());
+        int problem = Integer.parseInt(st.nextToken());
+        int score = Integer.parseInt(st.nextToken());
+
+        Node node = map.getOrDefault(id, new Node());
+        node.team = id;
+        node.submit++;
+        node.score.put(problem, Math.max(node.score.getOrDefault(problem, 0), score));
+        node.lastSubmit = j;
+
+        map.put(id, node);
+      }
+
+      List<Node> list = new ArrayList<>(map.values());
+      Collections.sort(list);
+
+      int rank = 0;
+      for (Node node : list) {
+        rank++;
+        if (node.team == t) {
+          sb.append(rank).append("\n");
+        }
+      }
+    }
+
+    System.out.println(sb);
+  }
+
+  static class Node implements Comparable<Node>{
+    int team, submit, lastSubmit;
+    Map<Integer, Integer> score = new HashMap<>();
+    public Node() {
+    }
+
+    @Override
+    public int compareTo(Node o) {
+      int total1 = this.score.values().stream().reduce(0, Integer::sum);
+      int total2 = o.score.values().stream().reduce(0, Integer::sum);
+      if (total1 == total2) {
+        if (this.submit == o.submit) {
+          return this.lastSubmit - o.lastSubmit;
+        }
+        return this.submit - o.submit;
+      }
+      return total2 - total1;
+    }
+  }
+}
---
+++ src/jchang/_0402/비슷한단어_2607.java
@@ -0,0 +1,58 @@
+package jchang._0402;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 비슷한단어_2607 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+
+    int result = 0;
+
+    String target = br.readLine();
+    int [] targetCount = new int[26];
+    for (int i = 0; i < target.length(); i++) {
+      targetCount[target.charAt(i) - 'A']++;
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+      int[] copy = targetCount.clone();
+      String str = br.readLine();
+      int count = 0;
+      for (int j = 0; j < str.length(); j++) {
+        if (copy[str.charAt(j) - 'A'] > 0 ){
+          count++;
+          copy[str.charAt(j) - 'A']--;
+        }
+      }
+
+      // 기준 문자열과 비교 문자열 길이가 같은 경우
+      if (str.length() == target.length()) {
+        // 기준 문자열과 같거나 기준 문자열에서 하나 바꿔끼운 경우만 비슷함
+        if (target.length() == count || target.length() - 1 == count) {
+          result++;
+        }
+      }
+
+      // 비교 문자열이 짧은 경우(제거)
+      if (str.length() == target.length() - 1) {
+        if (str.length() == count) {
+          result++;
+        }
+      }
+
+      // 비교 문자열이 큰 경우(추가)
+      if (str.length() == target.length() + 1) {
+        if (str.length() == count + 1) {
+          result++;
+        }
+      }
+    }
+
+
+    System.out.println(result);
+  }
+}

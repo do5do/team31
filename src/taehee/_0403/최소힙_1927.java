3019ca5e320d2774aa1ac333d38716d1b7da738b
thlee <e-tap@naver.com>
2024-04-03 16:43:01 +0900

    0403

---
+++ src/taehee/_0403/최소힙_1927.java
@@ -0,0 +1,26 @@
+package taehee._0403;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class 최소힙_1927 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    Queue<Integer> queue = new PriorityQueue<>();
+    for (int i = 0; i < N; i++) {
+      int x = Integer.parseInt(br.readLine());
+      if (x == 0) {
+        sb.append(queue.isEmpty() ? 0 : queue.poll()).append("\n");
+      } else {
+        queue.add(x);
+      }
+    }
+    System.out.println(sb);
+  }
+}
